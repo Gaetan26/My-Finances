d484a51d765424a1c6010e4ad2eb65b5c8f4e570
Gaetan Suenge <gaetansg26@gmail.com>
2026-04-10 10:57:07 +0100

    update sheet pydantic model

--- app/models/pydantic/sheet.py
+++ app/models/pydantic/sheet.py
@@ -5,7 +5,7 @@ from utils.datetime import format_date_now, format_time_now
 from enum import Enum
 import datetime
 
-class Motto(str, Enum):
+class Currency(str, Enum):
     CDF = "CDF"
     USD = "USD"
 
@@ -17,7 +17,7 @@ class Transaction(BaseModel):
     date: datetime.date = Field(default_factory=format_date_now)
     time: datetime.time = Field(default_factory=format_time_now)
     amount: int
-    motto: Motto
+    currency: Currency
     type: Type
     category: str
     description: str = ""
