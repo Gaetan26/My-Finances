e51ef23cf49aec0d00d4d271daa5ee372e3f0a22
Gaetan Suenge <gaetansg26@gmail.com>
2026-04-11 20:21:45 +0100

    update date format

--- app/utils/datetime.py
+++ app/utils/datetime.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 
 def format_date_now():
-    return datetime.now().strftime('%Y/%m/%d')
+    return datetime.now().strftime('%Y-%m-%d')
 
 def format_time_now():
     return datetime.now().strftime('%H:%M')
